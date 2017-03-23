c0cc004b8dd92942176ccfb995c807433a83106c
thonnycleuton <thonnycleuton@gmail.com>
2017-03-23 03:04:10 -0300

    mudanca de DB para PG

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -60,6 +60,19 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
+
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
