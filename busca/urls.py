03b7850fcef65c996fac4fba4f91a5148a67d25b
thonnycleuton <thonnycleuton@gmail.com>
2017-03-24 23:26:18 -0300

    remocao do haystack

--- busca/urls.py
+++ busca/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('',
                        (r'^selectable/', include('selectable.urls')),
-                       (r'^buscar/', include('haystack.urls')),
                        url(r'^$', BuscaView.as_view(), name='index'),
                        url(r'^criar/$', BuscaCreate.as_view(), name='create'),
                        url(r'^editar/(?P<pk>\d+)/$', BuscaUpdate.as_view(), name='edit'),
